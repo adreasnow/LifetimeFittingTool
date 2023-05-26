87e3e9e2aecf0afb031ff86f6a3842bd42d5a551
Adrea Snow <adrea.snow@gmail.com>
2023-05-26 14:59:03 +1000

    changed the window title

--- app/lifetimefitting/gui.py
+++ app/lifetimefitting/gui.py
@@ -192,7 +192,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Form", "Lifetime Fitting"))
         self.groupBox_4.setTitle(_translate("Form", "Input"))
         self.trf_browse.setText(_translate("Form", "Browse for TRF"))
         self.irf_browse.setText(_translate("Form", "Browse for IRF"))
